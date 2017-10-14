d5bd8019441c18f745d953069cea86a4cb6d2b70
Aneesh M Gangadhar <aneesh.mg2007@gmail.com>
2017-10-14 23:11:45 +0530

    integrated eslint and fixed lint issues

--- PromiseExample/server/server.js
+++ PromiseExample/server/server.js
@@ -1,13 +1,13 @@
 /* Simple server to be used for understanding promises */
 
 // Instantiating 'app' using currying!
-const app = require('express')()
-const handlers = require('./handlers')
+const app = require("express")()
+const handlers = require("./handlers")
 
-app.get('/', handlers.welcome)
-app.get('/_status', handlers._status)
-app.get('/handshake', handlers.handshake)
-app.get('/login/:num', handlers.login)
+app.get("/", handlers.welcome)
+app.get("/_status", handlers._status)
+app.get("/handshake", handlers.handshake)
+app.get("/login/:num", handlers.login)
 
 app.listen(3000)
 
