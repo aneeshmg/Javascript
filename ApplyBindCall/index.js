c914e37b4af6c50b6e9cb07f6b5fd4c675dde76f
Aneesh M Gangadhar <aneesh.mg2007@gmail.com>
2017-10-17 15:27:32 +0530

    add "apply()"

--- ApplyBindCall/index.js
+++ ApplyBindCall/index.js
@@ -1,13 +1,37 @@
-const User = {
+let User = {
     firstname: "John",
-    lastname: "Doe"
+    lastname: "Doe",
 }
-let greeting = null;
+const userAddress = [
+    "blah blah1",
+    "blah blah2",
+    "city",
+    "state, etc"
+]
+let greeting = null
 
 function generateGreeting(title) {
     return `${title}. ${this.firstname} ${this.lastname}`
 }
 
+function formatAddress(...addressLines) {
+    let address = []
+    let line = 0
+    for (addressLine of addressLines) {
+        // perform any check on the address line here
+        address.push(addressLine)
+        // Update the user object
+        this[`addressline${++line}`] = addressLine
+    }
+    // also return the formatted address
+    return address.join(', ')
+}
+
 // Usage of 'call()'
 greeting = generateGreeting.call(User, "Mr")
 console.log('Using "call()"', greeting)
+
+// Usage of 'apply()'
+address = formatAddress.apply(User, userAddress)
+console.log(address)
+console.log(User)
