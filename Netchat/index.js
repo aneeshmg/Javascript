eea21e69e9c29fea3cda7b076d54418186634e28
Aneesh M Gangadhar <aneesh.mg2007@gmail.com>
2017-11-16 14:42:58 +0530

    handle sockets better

--- Netchat/index.js
+++ Netchat/index.js
@@ -1,19 +1,27 @@
 'use strict'
 process.stdout.write("\u001B[2J\u001B[0;0f")
 const PORT = 9000
+let counter = 0
+let sockets = {}
 
 const server = require("net").createServer()
 
 server.on("connection", socket => {
+    socket.id = counter++
+    sockets[socket.id] = socket
+
     console.log("Client connected..")
     socket.write("Welcome fella\n")
 
     socket.on("data", data => {
-        console.log(`data: ${data}`)
-        socket.write(data)
+        Object.entries(sockets).forEach(([/*key*/, element]) => {
+            element.write(`${socket.id}: `)
+            element.write(data)
+        })
     })
 
     socket.on("end", () => {
+        delete sockets[socket.id]
         console.log("Client disconnected..")
     })
 })
