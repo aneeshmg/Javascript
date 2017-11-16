01129d9d543f88cc19c2a1d196760d84e2f78cab
Aneesh M Gangadhar <aneesh.mg2007@gmail.com>
2017-11-16 14:20:24 +0530

    TCP networking interface bootstrap

--- Netchat/index.js
+++ Netchat/index.js
@@ -1,3 +1,23 @@
+'use strict'
 process.stdout.write("\u001B[2J\u001B[0;0f")
+const PORT = 9000
 
 const server = require("net").createServer()
+
+server.on("connection", socket => {
+    console.log("Client connected..")
+    socket.write("Welcome fella\n")
+
+    socket.on("data", data => {
+        console.log(`data: ${data}`)
+        socket.write(data)
+    })
+
+    socket.on("end", () => {
+        console.log("Client disconnected..")
+    })
+})
+
+server.listen(PORT, () => {
+    console.log("Server started..")
+})
