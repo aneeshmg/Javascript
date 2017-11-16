d6ede369bf78b9d52fd4a02589da5025aa4a2feb
Aneesh M Gangadhar <aneesh.mg2007@gmail.com>
2017-11-16 15:34:24 +0530

    add structure for client/server of tasklist

--- Tasklist/client.js
+++ Tasklist/client.js
@@ -1,1 +1,20 @@
-// TODO: Client code...
+const EventEmitter = require("events")
+const readline = require("readline")
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const client = new EventEmitter()
+const server = require("./server")(client)
+
+server.on("response", response => {
+    process.stdout.write("\u001B[2J\u001B[0;0f") // equivalent of running clear()
+    process.stdout.write(response)
+    process.stdout.write("\n\> ")
+})
+
+rl.on("line", input => {
+    client.emit("command", input)
+})
--- Tasklist/server.js
+++ Tasklist/server.js
@@ -1,1 +1,41 @@
-// TODO: Server code..
+const EventEmitter = require("events")
+
+class Server extends EventEmitter {
+    constructor(client) {
+        super()
+        process.nextTick(() => {
+            this.emit("response", "Type a command (help to list commands)")
+        })
+
+        client.on("command", command => {
+            switch (command) {
+                case "help":
+                case "add":
+                case "delete":
+                case "ls":
+                    this[command]()
+                    break
+                default:
+                    this.emit("response", "Unknown command...")
+            }
+        })
+    }
+
+    help() {
+        this.emit("response", 'help...')
+    }
+
+    add() {
+        this.emit("response", 'add...')
+    }
+
+    delete() {
+        this.emit("response", 'delete...')
+    }
+
+    ls() {
+        this.emit("response", 'ls...')
+    }
+}
+
+module.exports = client => new Server(client)
