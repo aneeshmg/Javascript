39b02a986ae9934f8d7c0b0bbcaa34941fb9df38
Aneesh M Gangadhar <aneesh.mg2007@gmail.com>
2017-11-22 16:18:59 +0530

    finish right rotation

--- AandDS/ArrayRotation/index.js
+++ AandDS/ArrayRotation/index.js
@@ -1,7 +1,22 @@
 // Rotate an array 'k' times
 
 const rotate = (a, k) => {
-    return new Promise(resolve => resolve(a))
+    k = k % a.length
+    return new Promise(resolve => {
+        if (k == 0) resolve(a)
+        else {
+            let b = []
+            while(k > 0) {
+                for(let i = 0; i < a.length - 1; i++) {
+                    b[i + 1] = a[i]
+                }
+                b[0] = a[a.length - 1]
+                a = b.slice(0)
+                k--
+            }
+            resolve(b)
+        }
+    })
 }
 
 module.exports = rotate
--- AandDS/ArrayRotation/index.spec.js
+++ AandDS/ArrayRotation/index.spec.js
@@ -5,7 +5,37 @@ const utils = require("../utils/")
 
 let a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 let k = 0
+let aRotate1 = [9, 1, 2, 3, 4, 5, 6, 7, 8]
+let aRotate2 = [8, 9, 1, 2, 3, 4, 5, 6, 7]
+let aRotate3 = [7, 8, 9, 1, 2, 3, 4, 5, 6]
+let aRotate9 = a
+let aRotate10 = aRotate1
 
 rotate(a, k).then(result => {
     assert(utils.arraysEqual(result, a))
+}).catch(e => console.log(e))
+
+k = 1
+rotate(a, k).then(result => {
+    assert(utils.arraysEqual(result, aRotate1))
+}).catch(e => console.log(e))
+
+k = 2
+rotate(a, k).then(result => {
+    assert(utils.arraysEqual(result, aRotate2))
+}).catch(e => console.log(e))
+
+k = 3
+rotate(a, k).then(result => {
+    assert(utils.arraysEqual(result, aRotate3))
+}).catch(e => console.log(e))
+
+k = 9
+rotate(a, k).then(result => {
+    assert(utils.arraysEqual(result, aRotate9))
+}).catch(e => console.log(e))
+
+k = 10
+rotate(a, k).then(result => {
+    assert(utils.arraysEqual(result, aRotate10))
 }).catch(e => console.log(e))
