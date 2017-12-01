f916ca4f6e897245a6bb8b137d1b669fd842a10e
Aneesh M Gangadhar <aneesh.mg2007@gmail.com>
2017-12-01 12:25:33 +0530

    closest in array working

--- AandDS/Arrays/ClosestInArray.js
+++ AandDS/Arrays/ClosestInArray.js
@@ -1,10 +1,28 @@
 // A program to find the closest number to a given number in an array
 // Ex: given [5, 2, 13, 34, 12, 89, 50] and 10, output should be 12
+
+// Two approaches demonstrated to show the distinct them
 'use strict';
 
 module.exports = (list, number) => {
 
-    // TODO: program logic goes here
+    /* Using functional programming */
+    // Using map-reduce to pick the least of 'deltas'
+    // return Math.abs(
+    //     list
+    //     .map(e => Math.abs(number - e))
+    //     .reduce((acc, curr) => acc < curr ? acc : curr) - number)
 
-    return number
+
+    /* Using imperative approach */
+    let deltas = []
+    // First compute deltas
+    for(let i = 0; i < list.length; i++)
+        deltas.push(Math.abs(number - list[i]))
+
+    // Pick the least of deltas ie "closest"
+    let least = deltas.sort()[0]
+    let closest = Math.abs(least - number)
+
+    return closest
 }
--- AandDS/Arrays/ClosestInArray.spec.js
+++ AandDS/Arrays/ClosestInArray.spec.js
@@ -2,8 +2,11 @@ const program = require("./ClosestInArray")
 
 describe("Test cases for Closest in array program", () => {
 
-    // TODO: start TDD from here
-    it("program returns number", () => {
-        expect(program([1,2,3], 10)).toBe(10)
+    it("program returns closest number(1)", () => {
+        expect(program([1, 2, 3], 10)).toBe(3)
+    })
+
+    it("program returns closest number(2)", () => {
+        expect(program([10, 2, 31], 11)).toBe(10)
     })
 })
