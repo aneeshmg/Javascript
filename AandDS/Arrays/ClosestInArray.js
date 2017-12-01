feef66926966c8dd44226bef89d8fc315425ed80
Aneesh M Gangadhar <aneesh.mg2007@gmail.com>
2017-12-01 13:06:05 +0530

    refactor tests & code

--- AandDS/Arrays/ClosestInArray.js
+++ AandDS/Arrays/ClosestInArray.js
@@ -4,26 +4,30 @@
 // Two approaches demonstrated to show the distinct them
 'use strict';
 
-module.exports = (list, number) => {
+exports.Functional = (list, number) => {
 
     /* Using functional programming */
     // Using map-reduce to pick the least of 'deltas'
-    // const least = list
-    //     .map(e => Math.abs(number - e))
-    //     .reduce((acc, curr) => acc < curr ? acc : curr)
-    // return list.indexOf(least) !== -1? least + number : (number - least)
+    const least = list
+        .map(e => Math.abs(number - e))
+        .reduce((acc, curr) => acc < curr ? acc : curr)
+    return list.indexOf(least) !== -1? least + number : (number - least)
+
+}
+
+exports.Imperative = (list, number) => {
 
     // /* Using imperative approach */
     let deltas = []
     // First compute deltas
-    for(let i = 0; i < list.length; i++)
+    for (let i = 0; i < list.length; i++)
         deltas.push(Math.abs(number - list[i]))
 
     // Pick the least of deltas ie "closest"
     let least = deltas.sort()[0]
     let closest1 = (number - least)
     let closest2 = number + least
-    return list.indexOf(closest1) !== -1? closest1 : closest2
+    
+    return list.indexOf(closest1) !== -1 ? closest1 : closest2
 
-    return closest
 }
--- AandDS/Arrays/ClosestInArray.spec.js
+++ AandDS/Arrays/ClosestInArray.spec.js
@@ -1,24 +1,63 @@
-const program = require("./ClosestInArray")
+'use strict';
+let program;
 
 describe("Test cases for Closest in array program", () => {
 
-    it("program returns closest number(1)", () => {
-        expect(program([1, 2, 3], 10)).toBe(3)
+    describe("Test cases for Functional approach", () => {
+
+        beforeAll(() => {
+            return program = require("./ClosestInArray").Functional
+        })
+
+        it("program returns closest number(1)", () => {
+            expect(program([1, 2, 3], 10)).toBe(3)
+        })
+
+        it("program returns closest number(2)", () => {
+            expect(program([10, 2, 31], 11)).toBe(10)
+        })
+
+        it("program returns closest number(3)", () => {
+            expect(program([5, 2, 13, 34, 12, 89, 50], 10)).toBe(12)
+        })
+
+        it("program returns closest number(4)", () => {
+            expect(program([1, 1, 1, 1, 1], 10)).toBe(1)
+        })
+
+        it("program returns closest number(5)", () => {
+            expect(program([-1, -10, 21, 10, 11], 0)).toBe(-1)
+        })
+
     })
 
-    it("program returns closest number(2)", () => {
-        expect(program([10, 2, 31], 11)).toBe(10)
+
+    describe("Test cases for Imperative approach", () => {
+
+        beforeAll(() => {
+            return program = require("./ClosestInArray").Imperative
+        })
+
+        it("program returns closest number(1)", () => {
+            expect(program([1, 2, 3], 10)).toBe(3)
+        })
+
+        it("program returns closest number(2)", () => {
+            expect(program([10, 2, 31], 11)).toBe(10)
+        })
+
+        it("program returns closest number(3)", () => {
+            expect(program([5, 2, 13, 34, 12, 89, 50], 10)).toBe(12)
+        })
+
+        it("program returns closest number(4)", () => {
+            expect(program([1, 1, 1, 1, 1], 10)).toBe(1)
+        })
+
+        it("program returns closest number(5)", () => {
+            expect(program([-1, -10, 21, 10, 11], 0)).toBe(-1)
+        })
+
     })
 
-    it("program returns closest number(3)", () => {
-        expect(program([5, 2, 13, 34, 12, 89, 50], 10)).toBe(12)
-    })
-
-    it("program returns closest number(4)", () => {
-        expect(program([1, 1, 1, 1, 1], 10)).toBe(1)
-    })
-
-    it("program returns closest number(5)", () => {
-        expect(program([-1, -10, 21, 10, 11], 0)).toBe(-1)
-    })
 })
