773a6bb3a1d63d4502332b68feb801b32a26f7a5
Aneesh Gangadhar <aneesh.mg2007@gmail.com>
2020-02-10 17:50:36 -0700

    card game writing on

--- Games/CardTrick/game.js
+++ Games/CardTrick/game.js
@@ -1,33 +1,107 @@
 
 
 function initialize() {
-    let $container = document.getElementById('container')
-    let arena = document.getElementById('arena')
+    console.log('Initializing...')
 
-    let deck = Deck()
-    deck.mount($container)
 
-    deck.shuffle()
-    deck.bysuit()
-    deck.flip()
+    let cards = [
+        '10C',
+        '10D',
+        '10H',
+        '10S',
+        '2C',
+        '2D',
+        '2H',
+        '2S',
+        '3C',
+        '3D',
+        '3H',
+        '3S',
+        '4C',
+        '4D',
+        '4H',
+        '4S',
+        '5C',
+        '5D',
+        '5H',
+        '5S',
+        '6C',
+        '6D',
+        '6H',
+        '6S',
+        '7C',
+        '7D',
+        '7H',
+        '7S',
+        '8C',
+        '8D',
+        '8H',
+        '8S',
+        '9C',
+        '9D',
+        '9H',
+        '9S',
+        'AC',
+        'AD',
+        'AH',
+        'AS',
+        'JC',
+        'JD',
+        'JH',
+        'JS',
+        'KC',
+        'KD',
+        'KH',
+        'KS',
+        'QC',
+        'QD',
+        'QH',
+        'QS',
+        'TC',
+        'TD',
+        'TH',
+        'TS'
+    ]
 
-    console.log(deck.cards[0])
+    let html = cards.reduce((acc, curr) => acc += `<img id=${curr} class='card' src='cards/${curr}.svg'>`, '')
+    // TODO: Shuffle cards here
 
-    $container.addEventListener("click", function(e) {
-        console.log(e)
+    $('#pool').append(html)
+
+    selectCards()
+
+}
+
+function selectCards() {
+
+    let selectedCards = []
+
+    $('#pool .card').click(function() {
+        let id = $(this).attr('id')
+        selectedCards.push(id)
+
+        $(this).remove()
+
+        if (selectedCards.length == 3)
+            arrangeCards(selectedCards)
     })
+}
 
-    deck.cards.forEach(card => {
-        card.enableDragging()
+function arrangeCards(selectedCards) {
+    // Reset the pool
+    $('#pool').html('')
 
-        
-    })
-    console.log(deck)
+    let html = selectedCards.reduce((acc, curr, i) => {
+        switch (i % 3) {
+            case 0: acc += ``
+            case 1: acc += ``
+            case 2: acc += ``
+        }
+    }, '')
 
-    // console.log($container)
 
+    $('#pool').append(html)
 
-    console.log(deck.$el)
 }
 
 function Game() {
@@ -37,4 +111,4 @@ function Game() {
 
 }
 
-Game()
+$(document).ready(Game())
