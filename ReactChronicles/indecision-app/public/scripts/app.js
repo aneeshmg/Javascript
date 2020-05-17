afa21e4ae53b3a8e42d5603babb67d4c54a0705f
Aneesh Megravalli Gangadhar <aneesh.mg2007@gmail.com>
2020-05-17 12:27:08 -0700

    inject values into templates

--- ReactChronicles/indecision-app/public/scripts/app.js
+++ ReactChronicles/indecision-app/public/scripts/app.js
@@ -2,10 +2,70 @@
 
 console.log("hello");
 
+var app = {
+    title: "Indecision App",
+    subtitle: "Put your life in the hands of a computer"
+};
+
 var template = React.createElement(
-  "h1",
-  null,
-  "Hello world"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        app.title
+    ),
+    React.createElement(
+        "p",
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "First item"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Second item"
+        )
+    )
+);
+
+// var userName = "Aneesh"
+// var userAge = 26
+// var userLocation = "Tempe"
+
+var user = {
+    name: "Aneesh",
+    age: 26,
+    location: "Tempe"
+};
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        user.name
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: ",
+        user.age
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: ",
+        user.location
+    )
 );
 var appRoot = document.getElementById("app");
 
--- ReactChronicles/indecision-app/src/app.js
+++ ReactChronicles/indecision-app/src/app.js
@@ -1,6 +1,39 @@
 console.log("hello")
 
-var template = <h1>Hello world</h1>
+var app = {
+    title: "Indecision App",
+    subtitle: "Put your life in the hands of a computer"
+}
+
+var template = (
+    <div> 
+        <h1>{app.title}</h1> 
+        <p>{app.subtitle}</p>
+        <ol>
+            <li>First item</li>
+            <li>Second item</li>
+        </ol>
+    </div>
+)
+
+// var userName = "Aneesh"
+// var userAge = 26
+// var userLocation = "Tempe"
+
+var user = {
+    name: "Aneesh",
+    age: 26,
+    location: "Tempe"
+}
+
+
+var templateTwo = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+)
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
