d09bc663651fadbd73fa31cf3980e3e219689704
Aneesh Megravalli Gangadhar <aneesh.mg2007@gmail.com>
2020-05-17 11:24:00 -0700

    react hello world

---
+++ ReactChronicles/indecision-app/public/scripts/app.js
@@ -0,0 +1,6 @@
+console.log("hello")
+
+var template = /*#__PURE__*/React.createElement("p", null, "This is a paragraph");
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
