507c31583e9ae718ce1f06a98920a83f8e7a921e
Aneesh Megravalli Gangadhar <aneesh.mg2007@gmail.com>
2020-06-02 14:04:02 -0700

    initial components setup, with nesting

--- ReactChronicles/indecision-app/src/app.js
+++ ReactChronicles/indecision-app/src/app.js
@@ -1,60 +1,68 @@
-console.log("hello")
-
-const app = {
-    title: "Indecision App",
-    subtitle: "Put your life in the hands of a computer",
-    options: []
-}
-
-const nums = [99, 98, 97]
-
-const appRoot = document.getElementById("app");
-
-const renderIt = () => {
-    const template = (
-        <div>
-            <h1>{app.title}</h1>
-            {app.subtitle && <p>{app.subtitle}</p>}
-            <p>{app.options.length > 1 ? 'Here are options' : 'None'}</p>
-            <button disabled={app.options.length == 0} onClick={onMakeDecision}>What should I do?</button>
-            <button onClick={removeAll}>Remove all</button>
-            <ol>
-            {
-                app.options.map((option, i) => {
-                    return <li key={i}>{option}</li>
-                })
-            }
-            </ol>
-            <form onSubmit={onFormSubmit}>
-                <input type="text" name="option"></input>
-                <button>Add option</button>
-            </form>
-        </div>
-    )
-    ReactDOM.render(template, appRoot);
-}
-
-const removeAll = () => {
-    app.options = []
-    renderIt()
-}
-
-const onMakeDecision = e => {
-    const rand = Math.floor(Math.random() * app.options.length)
-    const option = app.options[rand]
-    alert(option)
-}
-const onFormSubmit = e => {
-    e.preventDefault();
-
-    const option = e.target.elements.option.value
-
-    if (option) {
-        app.options.push(option)
-        e.target.elements.option.value = ''
+class IndecisionApp extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header />
+                <Action />
+                <Options />
+                <AddOption />
+            </div>
+        )
     }
-    renderIt()
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>Put your life in the hands of a computer</h2>
+            </div>
+        )
+    }
+}
+
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should i do?</button>
+            </div>
+        )
+    }
+}
+
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                Options component
+                <Option />
+            </div>
+        )
+    }
+}
+
+class Option extends React.Component {
+    render() {
+        return (
+            <div>
+                Option Component here
+            </div>
+        )
+    }
+}
+
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                AddOption component
+            </div>
+        )
+    }
 }
 
 
-renderIt()
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
