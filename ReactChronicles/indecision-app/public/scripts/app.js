5059fdf6cdfdbdd35f3c1104a11707e3cd9bcfc0
Aneesh Megravalli Gangadhar <aneesh.mg2007@gmail.com>
2020-06-04 21:58:22 -0700

    add default states

--- ReactChronicles/indecision-app/public/scripts/app.js
+++ ReactChronicles/indecision-app/public/scripts/app.js
@@ -20,7 +20,7 @@ var IndecisionApp = function (_React$Component) {
         _this.handleClick = _this.handleClick.bind(_this);
         _this.handleOption = _this.handleOption.bind(_this);
         _this.state = {
-            options: []
+            options: props.options
         };
         return _this;
     }
@@ -55,12 +55,11 @@ var IndecisionApp = function (_React$Component) {
     }, {
         key: 'render',
         value: function render() {
-            var title = 'IndecisionApp';
             var subtitle = 'Put your life in the hands of a computer';
             return React.createElement(
                 'div',
                 null,
-                React.createElement(Header, { title: title, subtitle: subtitle }),
+                React.createElement(Header, { subtitle: subtitle }),
                 React.createElement(Action, {
                     hasOptions: this.state.options.length > 0,
                     handleClick: this.handleClick
@@ -77,6 +76,10 @@ var IndecisionApp = function (_React$Component) {
     return IndecisionApp;
 }(React.Component);
 
+IndecisionApp.defaultProps = {
+    options: []
+};
+
 var Header = function Header(props) {
     return React.createElement(
         'div',
@@ -86,12 +89,16 @@ var Header = function Header(props) {
             null,
             props.title
         ),
-        React.createElement(
+        props.subtitle && React.createElement(
             'h2',
             null,
             props.subtitle
         )
     );
+};
+
+Header.defaultProps = {
+    title: 'IndecisionApp'
 };
 
 var Action = function Action(props) {
@@ -189,4 +196,4 @@ var AddOption = function (_React$Component2) {
     return AddOption;
 }(React.Component);
 
-ReactDOM.render(React.createElement(IndecisionApp, null), document.getElementById('app'));
+ReactDOM.render(React.createElement(IndecisionApp, { options: ['abc', 'cde'] }), document.getElementById('app'));
--- ReactChronicles/indecision-app/src/app.js
+++ ReactChronicles/indecision-app/src/app.js
@@ -6,7 +6,7 @@ class IndecisionApp extends React.Component {
         this.handleClick = this.handleClick.bind(this)
         this.handleOption = this.handleOption.bind(this)
         this.state = {
-            options: []
+            options: props.options
         }
     }
 
@@ -37,11 +37,10 @@ class IndecisionApp extends React.Component {
         })
     }
     render() {
-        const title = 'IndecisionApp'
         const subtitle = 'Put your life in the hands of a computer'
         return (
             <div>
-                <Header title={title} subtitle={subtitle}/>
+                <Header subtitle={subtitle}/>
                 <Action 
                     hasOptions={this.state.options.length > 0} 
                     handleClick={this.handleClick}
@@ -56,13 +55,21 @@ class IndecisionApp extends React.Component {
     }
 }
 
+IndecisionApp.defaultProps = {
+    options: []
+}
+
 const Header = props => {
     return (
         <div>
             <h1>{props.title}</h1>
-            <h2>{props.subtitle}</h2>
+            {props.subtitle && <h2>{props.subtitle}</h2>}
         </div>
     )
+}
+
+Header.defaultProps = {
+    title: 'IndecisionApp'
 }
 
 const Action = props => {
@@ -132,4 +139,4 @@ class AddOption extends React.Component {
 
 
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
+ReactDOM.render(<IndecisionApp options={['abc', 'cde']}/>, document.getElementById('app'))
