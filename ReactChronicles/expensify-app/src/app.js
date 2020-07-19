6e8f6ee313b510e58009756d6bc18ac6d37b9f4d
Aneesh Megravalli Gangadhar <aneesh.mg2007@gmail.com>
2020-07-19 10:47:09 -0700

    integrate redux into expenses and refactor

--- ReactChronicles/expensify-app/src/app.js
+++ ReactChronicles/expensify-app/src/app.js
@@ -3,9 +3,27 @@ import ReactDOM from 'react-dom'
 
 import AppRouter from './router/AppRouter'
 
+import createStore from './store/configureStore'
+
+import { addExpense } from './actions/expenses'
+import { setTextFilter } from './actions/filters'
+import getVisibleExpenses from './selectors/expenses'
+
 import 'normalize.css/normalize.css'
 
 import './styles/styles.scss'
+
+const store = createStore()
+
+store.dispatch(addExpense({ description: 'water bill' }))
+store.dispatch(addExpense({ description: 'gas bill' }))
+store.dispatch(setTextFilter('water'))
+
+const state = store.getState()
+const expenses = getVisibleExpenses(state.expenses, state.filters)
+console.log(expenses, 'here')
+
+// console.log(store.getState())
 
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'))
