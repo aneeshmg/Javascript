32a148e7cbcb9be926ca6fb0a4b68033204266a6
Aneesh Megravalli Gangadhar <aneesh.mg2007@gmail.com>
2020-07-19 18:20:58 -0700

    refactor, move remove button

--- ReactChronicles/expensify-app/src/components/AddExpensePage.js
+++ ReactChronicles/expensify-app/src/components/AddExpensePage.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const AddExpensePage = () => (
+import ExpenseForm from './ExpenseForm'
+import { addExpense } from '../actions/expenses'
+
+const AddExpensePage = props => (
     <div>
-        This is add expense
+        <h1>Add expense</h1>
+        <ExpenseForm
+            onSubmit={expense => {
+                props.dispatch(addExpense(expense))
+                props.history.push('/')
+            }}
+        />
     </div>
 )
 
-export default AddExpensePage
+export default connect()(AddExpensePage)
--- ReactChronicles/expensify-app/src/components/ExpenseListItem.js
+++ ReactChronicles/expensify-app/src/components/ExpenseListItem.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import { connect } from 'react-redux'
 
-import { removeExpense } from '../actions/expenses'
-
-const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
+const ExpenseListItem = ({ id, description, amount, createdAt }) => (
     <div>
-        <h3>{description}</h3>
+        <Link to={`/edit/${id}`}>
+            <h3>{description}</h3>
+        </Link>
         <p>{amount} - {createdAt}</p>
-        <button onClick={() => {
-            dispatch(removeExpense({ id }))
-        }}>Remove</button>
     </div>
 )
 
