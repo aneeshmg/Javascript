986464a7228657d90be045075506d805be3ecde1
Aneesh Megravalli Gangadhar <aneesh.mg2007@gmail.com>
2020-07-19 18:30:58 -0700

    link to redux debugger

--- ReactChronicles/expensify-app/src/components/ExpenseListItem.js
+++ ReactChronicles/expensify-app/src/components/ExpenseListItem.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
     <div>
@@ -9,4 +10,4 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
     </div>
 )
 
-export default connect()(ExpenseListItem)
+export default ExpenseListItem
